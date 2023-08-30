cdacdce566e51a195283c7048b10dbd5ee5aa1ef
Sumon Hossain <sumonengr.96@gmail.com>
2023-08-30 18:01:24 +0600

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import appRouter from "./routes/endPoint.js";
-const app = express();
-const port = 3000; // You can change the port number if needed
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 // Define a route
 app.use("/api", appRouter);
 
+const port = process.env.PORT || 4000;
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port http://localhost:${port}`);
